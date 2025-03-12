c3b99d1b3d1072f8a15a6ba6c0334d73b32bf729
Lightnyng83 <159772828+Lightnyng83@users.noreply.github.com>
2025-03-12 12:07:09 +0100

    Add - Docker & NuggetPackage

--- PatientService/Patient.Data/obj/Debug/net9.0/Patient.Data.AssemblyInfo.cs
+++ PatientService/Patient.Data/obj/Debug/net9.0/Patient.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patient.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fcf4d0d6ed108f334ab73653d4fe55d691bfda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patient.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patient.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
